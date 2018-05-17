c6d29da0feb5f8bcaf51480b4177fc5949c38ccf
Zaria Mateen <zmateen43@gmail.com>
2018-05-17 15:09:09 -0400

    <COMMENT>

---
+++ assets/javascript/game.js
@@ -0,0 +1,114 @@
+// Random number generator 
+$(document).ready(function(){
+    var random = Math.floor(Math.random() * 102 + 19)
+    
+    // The random number
+    $("#targetScore").text(random);
+    
+    // Get random number for each crystal
+    var num1 = Math.floor(Math.random() * 12 + 1)
+    var num2 = Math.floor(Math.random() * 12 + 1)
+    var num3 = Math.floor(Math.random() * 12 + 1)
+    var num4 = Math.floor(Math.random() * 12 + 1)
+    
+    // Player's score
+    var yourScore = 0;
+  
+  // Resetting
+  function reset() {
+
+        random = Math.floor(Math.random() * 102 + 19);
+        console.log(Random)
+
+        $("#targetScore").text(random);
+
+        num1= Math.floor(Math.random() * 12 + 1);
+        num2= Math.floor(Math.random() * 12 + 1);
+        num3= Math.floor(Math.random() * 12 + 1);
+        num4= Math.floor(Math.random() * 12 + 1);
+
+        yourScore = 0;
+
+        $("#targetScore").text(yourScore);
+
+        } 
+
+  // Displaying wins
+  function yay(){
+  alert("Ultimate power! You win!");
+    wins++; 
+    $('#wins').text(wins);
+    reset();
+  }
+
+  // Displaying losses
+  function boo(){
+  alert ("Lost In Space! You lose!");
+    losses++;
+    $('#losses').text(losses);
+    reset()
+  }
+
+
+  // Clicking on the crystals
+    $("#one").on ("click", function() {
+
+      yourScore = yourScore + num1;
+      console.log("New yourScore= " + yourScore);
+      $("#targetScore").text(yourScore); 
+
+
+        //Win & lose if/else
+          if (yourScore == random) {
+            yay();
+          }
+          else if (yourScore > random) {
+            boo();
+          }   
+    });
+    
+    
+    $("#two").on ('click', function() {
+
+      yourScore = yourScore + num2;
+      console.log("New yourScore= " + yourScore);
+      $("#targetScore").text(yourScore); 
+
+          if (yourScore == random) {
+            yay();
+          }
+          else if ( yourScore > random) {
+            boo();
+          } 
+    });
+    
+    
+    $("#three").on ("click", function() {
+
+      yourScore = yourScore + num3;
+      console.log("New yourScore= " + yourScore);
+      $("#targetScore").text(yourScore);
+  
+            if (yourScore == random) {
+            yay();
+          }
+          else if ( yourScore > random) {
+            boo();
+          } 
+    });
+    
+    
+    $("#four").on ("click", function() {
+      yourScore = yourScore + num4;
+      console.log("New yourScore= " + yourScore);
+      $("#targetScore").text(yourScore); 
+        
+            if (yourScore == random) {
+            yay();
+          }
+          else if ( yourScore > random) {
+            boo();
+          }
+    });   
+  }); 
+  
